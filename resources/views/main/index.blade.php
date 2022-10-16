8bee2c15e3d9b8f688714a66711446018c62a5ca
egor gaychuk <egor.gaych@gmail.com>
2022-10-16 22:41:11 +0300

    hot fix

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 @section('title', $page->title)
+@section('description', $page->description)
 @section('content')
     @if ($page->url == 'home')
         @include('includes.last-adverts-block')
