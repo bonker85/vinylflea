1a9f577ecd2848bbbd0aa2f2a59e99ef83d5f6c5
egor gaychuk <egor.gaych@gmail.com>
2022-10-16 22:17:23 +0300

    'sdfds'

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+@section('title', 'Ошибка 404')
+@section('content')
+    <section class="py-3 border-bottom border-top d-md-flex bg-light">
+        <div class="container">
+            <div class="page-breadcrumb d-flex align-items-center">
+                <h3 class="breadcrumb-title pe-3">Что-то пошло не так</h3>
+                <div class="ms-auto">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb mb-0 p-0">
+                            <li class="breadcrumb-item"><a href="/">Главная</a>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="error-404 text-center">
+        <h2>500</h2>
+    </div>
+@endsection
