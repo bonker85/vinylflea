ef39668b6e0154e2ab2b03afd5dea5a5c18e1ca5
egor gaychuk <egor.gaych@gmail.com>
2022-10-26 01:25:23 +0300

    telegram

--- app/helpers.php
+++ app/helpers.php
@@ -83,4 +83,23 @@ function thumb_url($url, $item)
     }
     return $url;
 }
+
+function message_to_telegram($text)
+{
+    $ch = curl_init();
+    curl_setopt_array(
+        $ch,
+        array(
+            CURLOPT_URL => 'https://api.telegram.org/bot' . env("TELEGRAM_TOKEN ") . '/sendMessage',
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_POSTFIELDS => array(
+                'chat_id' => env('TELEGRAM_CHAT'),
+                'text' => $text,
+            ),
+        )
+    );
+    curl_exec($ch);
+}
 ?>
