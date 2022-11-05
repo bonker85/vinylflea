9b2a31fe72b6e146f5038089779e56349cfef8c8
egor gaychuk <egor.gaych@gmail.com>
2022-11-06 02:40:11 +0300

    sitemap add artist

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Advert;
+use App\Models\DiscogsArtist;
 use App\Models\Page;
 use App\Models\Style;
 use Illuminate\Http\Request;
@@ -21,6 +22,11 @@ class SitemapController extends Controller
         $sitemap = Sitemap::create();
         //добавление главной
         $sitemap->add(Url::create('/')->setLastModificationDate($lastModificationDate));
+        //добавление артистов
+        $artists = DiscogsArtist::select('discogs_artist_id')->get();
+        foreach ($artists as $artist) {
+            $sitemap->add(Url::create(route('artist', $artist->discogs_artist_id))->setLastModificationDate($lastModificationDate));
+        }
         //Добавление новостей
         $sitemap->add(Url::create('/news')->setLastModificationDate($lastModificationDate));
         $pages = Page::select('url')->where('status', 1)->where('parent_id', 2)->get();
