bfb7e490faea9b3d8fcab023e1472dca8540830e
egor gaychuk <egor.gaych@gmail.com>
2022-11-06 14:22:22 +0300

    mt message

--- resources/views/includes/message.blade.php
+++ resources/views/includes/message.blade.php
@@ -11,7 +11,7 @@
     </div>
 @endif
 @if (auth()->check() && $countViewMessages)
-    <div class="container alert alert-warning alert-dismissible fade show" role="alert">
+    <div class="message-block container alert alert-warning alert-dismissible fade show mt-4" role="alert">
         <a class="link-messages" href="{{route('profile.messages')}}">У Вас есть непрочитанные сообщения</a>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
