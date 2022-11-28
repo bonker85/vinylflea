7e4791242241205ccbbc5cf3e7930ed35f008200
egor gaychuk <egor.gaych@gmail.com>
2022-11-29 02:09:47 +0300

    fix

--- public/assets/js/sw.js
+++ public/assets/js/sw.js
@@ -16,7 +16,7 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
     event.waitUntil(
         caches.open(CACHE)
-            .then((cache) => cache.add(offlineFallbackPage))
+            .then((cache) => {console.log(offlineFallbackPage);cache.add(offlineFallbackPage);})
     );
 });
 
