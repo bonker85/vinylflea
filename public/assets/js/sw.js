229d0e24955df8a3288e22cac20151a07896c4d8
egor gaychuk <egor.gaych@gmail.com>
2022-11-29 02:27:21 +0300

    fix

--- public/assets/js/sw.js
+++ public/assets/js/sw.js
@@ -1,47 +1,11 @@
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "https://vinylflea.by/404";
-
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
+self.addEventListener('install', (event) => {
+    console.log('Установлен');
 });
 
-self.addEventListener('install', async (event) => {
-    event.waitUntil(
-        caches.open(CACHE)
-            .then((cache) => {console.log(offlineFallbackPage); cache.add(offlineFallbackPage);})
-    );
+self.addEventListener('activate', (event) => {
+    console.log('Активирован');
 });
-
-if (workbox.navigationPreload.isSupported()) {
-    workbox.navigationPreload.enable();
-}
 
 self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
-            try {
-                const preloadResp = await event.preloadResponse;
-
-                if (preloadResp) {
-                    return preloadResp;
-                }
-
-                const networkResp = await fetch(event.request);
-                return networkResp;
-            } catch (error) {
-
-                const cache = await caches.open(CACHE);
-                const cachedResp = await cache.match(offlineFallbackPage);
-                return cachedResp;
-            }
-        })());
-    }
+    console.log('Происходит запрос на сервер');
 });
