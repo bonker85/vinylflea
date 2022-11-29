88de58c8cbf658315911bb85574ec69f4c531a75
egor gaychuk <egor.gaych@gmail.com>
2022-11-29 16:41:22 +0300

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
         <link rel="manifest"  href="{{ asset('manifest.json') }}">
         <script>
             if (typeof navigator.serviceWorker !== 'undefined') {
-                navigator.serviceWorker.register('/assets/js/sw.js')
+                navigator.serviceWorker.register('sw.js')
             }
         </script>
     @endif
