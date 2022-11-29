6c7aecc3a19d6bcc10749ff156d7b83bc8507b68
egor gaychuk <egor.gaych@gmail.com>
2022-11-29 19:19:44 +0300

    fix

--- public/sw.js
+++ public/sw.js
@@ -9,3 +9,15 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
     console.log('Происходит запрос на сервер');
 });
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+    // Optionally, send analytics event that PWA install promo was shown.
+    console.log(`'beforeinstallprompt' event was fired.`);
+});
