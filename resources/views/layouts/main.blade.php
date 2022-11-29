15af02ac84b61caabae70c81b51ba37722980cb4
egor gaychuk <egor.gaych@gmail.com>
2022-11-29 12:43:18 +0300

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
         <link rel="manifest" href="/manifest.json">
         <script>
             if (typeof navigator.serviceWorker !== 'undefined') {
-                navigator.serviceWorker.register('https://vinylflea.by/assets/js/sw.js')
+                navigator.serviceWorker.register('/assets/js/sw.js')
             }
         </script>
     @endif
