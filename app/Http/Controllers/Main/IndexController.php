1549f4121f1bb96bc66d484e57259ac88740ccfd
egor gaychuk <egor.gaych@gmail.com>
2023-01-10 23:42:37 +0300

    sell vinyl

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Main;
 //use DB;
 //use Illuminate\Support\Facades\Hash;
 
+use App\Mail\SellRecords;
 use App\Models\Advert;
 use App\Models\Page;
 use App\Models\Style;
 use App\Services\AdvertService;
 use App\Services\ProfileService;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Mail;
 
 
 class IndexController extends BaseController
@@ -19,12 +22,23 @@ class IndexController extends BaseController
      //   $profileSevert = new ProfileService();
       // dd($profileSevert->createAdvertThumbnail('/rule/main.jpg'));
    //     DB::table('users')->insert(['name'=>'Egor','email'=>'bonker85@mail.ru','password'=>Hash::make('fishki182')]);
+        if ($url == 'sell-records') {
+            return view('sell-records');
+        }
         $page = $this->service->getPage($url);
         $lastAdvertsList = Advert::select()
             ->where('status', AdvertService::getStatusByName('activated'))
             ->orderBy('up_time', 'DESC')->limit(12)->get();
         $styles = Style::select()->orderBy('name')->get();
         return view('main.index', compact('page', 'lastAdvertsList', 'styles'));
+    }
+
+    public function sellRecords()
+    {
+        if (request()->message) {
+            Mail::to(env('MAIL_FROM_ADDRESS'))->send(new SellRecords(['message' => request()->message]));
+        }
+        return view('sell-records', ['success' => true]);
     }
 
 }
---
+++ resources/views/sell-records.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+@section('title', "Продать виниловые пластинки")
+@section('description', "Продать виниловые пластинки")
+@section('content')
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    @if (isset($success))
+                        <p class="success-sell-send">Данные приняты в обработку. Скоро с Вами свяжутся. Спасибо!</p>
+                    @else
+                    <img width="100%" src="{{asset('assets/images/sell-vinyl.png')}}"/>
+                    <div class="form-sell-vinyl">
+                        <form method="post" action="{{route('main.sell-records')}}">
+                            @csrf
+                            @method('POST')
+                            <textarea name="message" placeholder="Оставьте ваши контактные данные такие как (телефон, viber или email), а также адрес, состояние пластинок  и мы свяжемся с Вами в ближайшее время, Cпасибо!"></textarea>
+                            <input type="submit" class="btn btn-success"/>
+                        </form>
+                    </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
