c46522998e91a3bc0b5e1c8e77d290e1442042b6
egor gaychuk <egor.gaych@gmail.com>
2023-11-24 14:10:01 +0300

    h1

--- resources/views/includes/last-adverts-block.blade.php
+++ resources/views/includes/last-adverts-block.blade.php
@@ -1,5 +1,8 @@
 <section class="adverts-section">
     <div class="container">
+        @if (Request::is('/'))
+            <h1 class="header-in-menu">Виниловые Пластинки</h1>
+        @endif
         <div class="d-flex align-items-center all-vinyl-list pt-3">
             <span class="text-uppercase fs-4 mt-0 mb-0">Обновления</span>
             <a href="{{route('vinyls.style', 'all')}}" style="margin-top: 2px;" class="button-st text-uppercase   ms-auto text-dark">Смотреть все<i class='bx bx-chevron-right'></i></a>
