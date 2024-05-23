ff8168e4c40c4f9d51b39cb95dd4f78c8e77edf0
egor gaychuk <egor.gaych@gmail.com>
2024-05-23 13:45:08 +0300

    fix ref

--- resources/views/includes/banner-xbet.blade.php
+++ resources/views/includes/banner-xbet.blade.php
@@ -1,5 +1,5 @@
 <section >
     <div class="container d-flex justify-content-center align-items-center p-3">
-        <iframe scrolling='no' frameBorder='0' style='padding:0px; margin:0px; border:0px;border-style:none;border-style:none;' width='800' height='90' src="https://refpa4293501.top/L?tag=s_3345794m_1234c_&site=3345794&ad=1234" ></iframe>
+        <iframe scrolling='no' frameBorder='0' style='padding:0px; margin:0px; border:0px;border-style:none;border-style:none;' width='800' height='90' src="https://refpa6781648.top/I?tag=s_3345794m_3864c_&site=3345794&ad=3864" ></iframe>
     </div>
 </section>
